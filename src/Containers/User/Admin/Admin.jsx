f818540335b0630b70fb0f7e475b6885f040f876
Germanilu <lucianogermani93@gmail.com>
2022-07-10 12:05:19 +0200

    Feat: Created design admin panel

--- src/Containers/User/Admin/Admin.jsx
+++ src/Containers/User/Admin/Admin.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import './Admin.scss'
  
 const Admin = () => {
      return (
-         <div className='adminDesign'>Bienvenido Admin!</div>
+         <div className='adminDesign'>
+            <div className="adminPanel">
+
+                <div className='adminRow'>
+                <p>Buscar todos los usuarios</p>
+                <input type="text" />
+                </div>
+                <div className='adminRow'>
+                <p>Buscar todas las ordenes</p>
+                <input type="text" />
+                </div>
+                <div className='adminRow'>
+                <p>Buscar ordenes por usuario</p>
+                <input type="text" />
+                </div>
+
+            </div>
+
+            <div className="adminResult">
+                Hola soy el resultado
+            </div>
+
+         </div>
      )
 }
 export default Admin;
